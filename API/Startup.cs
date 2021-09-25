79d895fdc13f01f186d640d128560734f5e53481
Adriano dos Anjos <adferreiraneto@gmail.com>
2021-09-25 03:36:24 -0300

    realizado Angular da classe Carro

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,6 +28,15 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Configurar a politica de CORS para receber requisições de qualquer origem
+            services.AddCors(
+
+                options => {
+                    options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin());
+                }
+            );
+
 
             services.AddDbContext<Banco>
             (
@@ -49,6 +58,8 @@ namespace API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
+
+            app.UseCors("CorsPolicy");
 
             app.UseHttpsRedirection();
 
---
+++ FRONT/src/app/services/carro.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Carro } from './../models/carro';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CarroService {
+
+    private baseURL = "http://localhost:5000/api";
+
+    constructor(private http: HttpClient) {}
+
+    list(): Observable<Carro[]> {
+        return this.http.get<Carro[]>(`${this.baseURL}/carro/relacaodecarros`);
+    }
+}
