c0f6adbd65d16bc0bc2e7d1358e7548eb94d2015
MaiconBusinari <maiconlucascamposbusinari@gmail.com>
2021-09-26 17:29:51 -0300

    Criado a pasta de componets

--- FRONT/src/app/app.module.ts
+++ FRONT/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ListarCarroComponent } from './components/views/carro/listar-carro/listar-carro.component';
+import { CadastrarCarroComponent } from './components/views/carro/cadastrar-carro/cadastrar-carro.component';
+import { PessoaListarComponent } from './components/views/pessoa/pessoa-listar/pessoa-listar.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        ListarCarroComponent,
+        CadastrarCarroComponent,
+        PessoaListarComponent
     ],
     imports: [
         BrowserModule,
