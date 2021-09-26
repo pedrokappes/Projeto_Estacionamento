b92dc2286ee7d034ecf0780fbb3bbbaa6bf16873
pedrokappes <pedrokappes@gmail.com>
2021-09-26 15:08:58 -0300

    criação do service e model de pessoa

---
+++ FRONT/src/app/models/pessoa.ts
@@ -0,0 +1,7 @@
+export interface Pessoa {
+    
+    nome: string;
+    cpf: string;
+    telefone: string;
+
+}
--- FRONT/src/app/services/carro.service.ts
+++ FRONT/src/app/services/carro.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Carro } from './../models/carro';
+import { Carro } from '../models/carro';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
 
