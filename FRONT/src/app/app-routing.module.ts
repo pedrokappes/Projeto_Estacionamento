5df64d900dd6416e46d85ed098f6abf81faeb482
pedrokappes <pedrokappes@gmail.com>
2021-09-28 18:22:40 -0300

    Configurado primeiras rotas

--- FRONT/src/app/app-routing.module.ts
+++ FRONT/src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListarCarroComponent } from './components/views/carro/listar-carro/listar-carro.component';
+import { PessoaListarComponent } from './components/views/pessoa/pessoa-listar/pessoa-listar.component';
+import { VagaListarComponent } from './components/views/vaga/vaga-listar/varga-listar.component';
 
 const routes: Routes = [
-    
+    {
+        path: "",
+        component: VagaListarComponent,
+    },
+    {
+        path: "pessoaListar",
+        component: PessoaListarComponent,
+    },
+    {
+        path: "carro/listar",
+        component: ListarCarroComponent,
+    },
+    {
+        path: "vagalistar",
+        component: VagaListarComponent,
+    },
 ];
 
 @NgModule({
