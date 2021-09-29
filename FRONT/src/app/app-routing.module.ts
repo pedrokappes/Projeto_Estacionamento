5ccdf0f3aef9a0f06d36f109d8dc5ea430f67ad8
pedrokappes <pedrokappes@gmail.com>
2021-09-28 23:07:26 -0300

    Implementado Listar vagas e  cadastrar pessoa

--- FRONT/src/app/app-routing.module.ts
+++ FRONT/src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarCarroComponent } from './components/views/carro/cadastrar-carro/cadastrar-carro.component';
 import { ListarCarroComponent } from './components/views/carro/listar-carro/listar-carro.component';
+import { CadastrarPessoaComponent } from './components/views/pessoa/cadastrar-pessoa/cadastrar-pessoa.component';
 import { PessoaListarComponent } from './components/views/pessoa/pessoa-listar/pessoa-listar.component';
-import { VagaListarComponent } from './components/views/vaga/vaga-listar/varga-listar.component';
+import { VagaListarComponent } from './components/views/vaga/vaga-listar/vaga-listar.component';
 
 const routes: Routes = [
     // {
@@ -30,6 +31,10 @@ const routes: Routes = [
         path: "vagalistar",
         component: VagaListarComponent,
     },
+    {
+        path: "pessoa/cadastrar",
+        component: CadastrarPessoaComponent,
+    }
 ];
 
 @NgModule({
