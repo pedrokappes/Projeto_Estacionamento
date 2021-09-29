e674f066da7304c13f620b374df885d8db4cfb18
MaiconBusinari <maiconlucascamposbusinari@gmail.com>
2021-09-28 22:33:18 -0300

    Implementado cadastro de carro no angular

--- FRONT/src/app/app-routing.module.ts
+++ FRONT/src/app/app-routing.module.ts
@@ -1,22 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarCarroComponent } from './components/views/carro/cadastrar-carro/cadastrar-carro.component';
 import { ListarCarroComponent } from './components/views/carro/listar-carro/listar-carro.component';
 import { PessoaListarComponent } from './components/views/pessoa/pessoa-listar/pessoa-listar.component';
 import { VagaListarComponent } from './components/views/vaga/vaga-listar/varga-listar.component';
 
 const routes: Routes = [
+    // {
+    //     path: "",
+    //     component: VagaListarComponent,
+    // },
     {
-        path: "",
-        component: VagaListarComponent,
-    },
-    {
-        path: "pessoaListar",
+        path: "pessoa/listar",
         component: PessoaListarComponent,
     },
     {
         path: "carro/listar",
         component: ListarCarroComponent,
     },
+    {
+        path: "carro/cadastrar",
+        component: CadastrarCarroComponent,
+    },
+    // {
+    //     path: "carro/consultar",
+    //     component: BuscarPlacaCarroComponent,
+    // },
     {
         path: "vagalistar",
         component: VagaListarComponent,
