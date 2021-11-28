d3e1100cf3a922beacdfb6bda5d408a24c26aa89
MaiconBusinari <Maiconlucascamposbusinari@gmail.com>
2021-11-27 22:37:49 -0300

    Implementado saida de veiculos

--- API/Models/Carro.cs
+++ API/Models/Carro.cs
@@ -13,7 +13,7 @@ namespace API.Models
         public string Placa { get; set; }
         public string Cor { get; set; }        
         public int PessoaId { get; set; }
-        public Pessoa Pessoa { get; set; }    
+        public Pessoa Pessoa { get; set; }     
         public DateTime HoraEntrada { get; set;}
     }
 }
